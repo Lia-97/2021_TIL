69e2eba0d0a3fe20049a79b9c46246b79a2a88ab
Jihye Lee <zhezhe0206@naver.com>
2021-01-20 12:54:43 +0900

    edit

--- algorithm_programmers/level1.py
+++ algorithm_programmers/level1.py
@@ -19,3 +19,27 @@
 #         return False
 #
 # print(solution('Pyy'))
+
+# # 두 정수 사이의 합
+#
+# def solution(a, b):
+#     answer = 0
+#     if a <= b:
+#         for i in range(a, b+1):
+#             answer += i
+#     if a > b:
+#         for i in range(b, a+1):
+#             answer += i
+#     return answer
+#
+# print(solution(5,3))
+#
+# # 두 정수 사이의 합 (또 다른 풀이)
+#
+# def solution(a, b):
+#     if a > b:
+#         a, b = b, a
+#     return sum(range(a, b+1))
+
+# 문자열 내 마음대로 정렬하기
+
