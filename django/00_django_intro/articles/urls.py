ac3ca96cf6e6f41223f128fd2812e2dfe7365a8b
Jihye Lee <zhezhe0206@naver.com>
2021-03-08 19:01:34 +0900

    reset

--- django/00_django_intro/articles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views # 현재 폴더에 있는 views
-
-urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('ping/', views.ping, name='ping'),
-    path('pong/', views.pong, name='pong'),
-    path('<int:num>/', views.detail, name='detail'),
-]
